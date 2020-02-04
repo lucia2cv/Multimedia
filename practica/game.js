7ad9272628bb557db5476ad15a9d410ae625053d
lucia2cv <43386417+lucia2cv@users.noreply.github.com>
2020-02-04 17:53:29 +0100

    cargar assets

--- practica/game.js
+++ practica/game.js
@@ -5,6 +5,11 @@ $(window).load(function(){
 var game = {
 	//comenzar inicializacion de objetos, precarga elementos y pantalla de inicio
 	init: function(){
+		//inicializar objetos
+		levels.init();
+
+		/*inicializar assets
+		loader.init();*/
 		//ocultar todas las capas del juego menos la pantalla de inicio
 		$('.gamelayer').hide();
 		$('#gamestartscreen').show();
@@ -14,4 +19,103 @@ var game = {
 		game.canvas = $('#gamecanvas')[0];
 		game.context = game.canvas.getContext('2d');
 	},
+	showLevelScreen:function(){
+		$('#gamestartscreen').hide();
+		$('#levelselectscreen').show('slow');
+	},
+
 }
+
+var levels = {
+	data:[
+		//primer nivel
+		{
+			foreground: 'desert-foreground',
+			background: 'clouds-background',
+			entities: []
+		},
+		//segundo nivel
+		{
+			foreground: 'desert-foreground',
+			background: 'clouds-background',
+			entities: []
+		}
+
+	],
+	// inicializar pantalla de seleccion
+
+	init: function(){
+		var html = "";
+		for (var i=0; i<levels.data.length; i++){
+			var level = levels.data[i];
+			html += '<input type="button" value="' + (i+1)+'">';
+		};
+		$('#levelselectscreen').html(html);
+		//controladores de eventos
+		$('#levelselectscreen input').click(function(){
+			levels.load(this.value-1);
+			$('#levelselectscreen').hide();
+		});
+	},	
+
+	//cargar todos los datos
+	load:function(number){
+
+	}
+}	
+/*var loader = {
+	loaded:true,
+	loadedCount: 0, //contador de assets cargados antes
+	totalCount:0, //assets que es necesario cargar
+
+	init:function(){
+
+		var mp3Support, oggSupport;
+		var audio = document.createElement('audio');
+		if(audio.canPlayType){
+			mp3Support = "" != audio.canPlayType('audio/mpeg');
+			oggSupport = "" != audio.canPlayType('audio/ogg; codecs="vorbis"');
+
+		}else{
+			mp3Support = false;
+			oggSupport = false;
+		}
+
+		loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;
+	},
+
+	loadImage:function(url){
+		this.totalCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var image = new Image();
+		image.src = url;
+		image.onload = loader.itemLoaded;
+		return image;
+	},
+
+	soundFileExtn: ".ogg",
+	loadSound: function(url){
+		this.totalCount++;
+		this.loaded = false;
+		$('#loadingscreen').show();
+		var audio = new Audio();
+		audio.src = url +loader.soundFileExtn;
+		audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+		return audio;
+	},
+	itemLoaded:function(){
+		loader.loadedCount++;
+		$('#loadingmessage').html('Loaded ' + loader.loadedCount+' of '+loader.totalCount);
+		if ((loader.loadedCount === loader.totalCount){ 
+
+			loader.loaded = true;
+			$('#loadingscreen').hide();
+
+			if(loader.onload){
+				loader.onload();
+				loader.onload = undefined;
+			}
+		}
+	}
+}*/
